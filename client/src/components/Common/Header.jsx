f11720f54ddd817d38c63553327e9ca8e5f15ed6
ryan-kim-dev <ryankim.h.dev@gmail.com>
2022-09-18 13:11:27 +0900

    design: 헤더 로고 스타일링

--- client/src/components/Common/Header.jsx
+++ client/src/components/Common/Header.jsx
@@ -1,8 +1,23 @@
 import React from 'react';
+import { Navigate, useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
+import Logo from '../../assets/android-chrome-512x512.png';
 
 function Header() {
-  return <HeaderLayout>Header</HeaderLayout>;
+  const navigate = useNavigate();
+
+  return (
+    <HeaderLayout>
+      <HeaderTitleBox onClick={() => navigate('/')}>
+        <HeaderTitleLogoBox>
+          <HeaderTitleLogoImg src={Logo} />
+        </HeaderTitleLogoBox>
+        <HeaderTitleText>
+          <i>Lit Cars</i>
+        </HeaderTitleText>
+      </HeaderTitleBox>
+    </HeaderLayout>
+  );
 }
 
 export default Header;
@@ -13,5 +28,28 @@ const HeaderLayout = styled.div`
   width: 100%;
   height: 50px;
   background-color: #fff;
-  border: 1px solid black;
+  border-top: 1px solid black;
+  border-bottom: 1px solid black;
+  display: flex;
+  justify-content: center;
 `;
+
+const HeaderTitleBox = styled.div`
+  font-family: 'League Gothic', sans-serif;
+  width: 150px;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  column-gap: 10px;
+  line-height: 1;
+`;
+
+const HeaderTitleLogoBox = styled.div``;
+
+const HeaderTitleLogoImg = styled.img`
+  width: 30px;
+`;
+
+const HeaderTitleText = styled.h1``;
